41d024aa06589cc24a462ddde6a132cf400c8457
nsmedley <smed121@gmail.com>
2019-02-22 09:40:08 +0000

    Intial Commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  cssnano = require("cssnano"),
+  postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  sourcemaps = require("gulp-sourcemaps"),
+  browserSync = require("browser-sync").create();
+
+function style() {
+  return (
+    gulp
+      .src("*.scss")
+      .pipe(sourcemaps.init())
+      .pipe(sass())
+      .on("error", sass.logError)
+      .pipe(postcss([autoprefixer(), cssnano()]))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(""))
+      .pipe(browserSync.stream())
+  );
+}
+
+function reload() {
+  browserSync.reload();
+}
+
+function watch(){
+  browserSync.init({
+    server: {
+        baseDir: "./"
+    }
+  });
+  gulp.watch('*.scss', style);
+  gulp.watch("*.html", reload);
+  gulp.watch("js/*.js", reload);
+}
+
+exports.style = style;
+exports.watch = watch
---
+++ js/ppsCheckout.js
@@ -0,0 +1,56 @@
+$(function() {
+    hideInputLabels();
+    showPaymentDrawer();
+    cardInputMasks();
+    cardPreviewAndFlip();
+});
+
+//If input field has a value then hide the label
+function hideInputLabels() {
+    $('input').focusout(function(){
+        var fieldId = $(this).attr("id");
+        if ($(this).val() == ''){
+            $("label[for='"+fieldId+"']").removeClass('stay');
+         } else {
+            $("label[for='"+fieldId+"']").addClass('stay');
+         }
+    });
+}
+
+//Check to make sure details inputs are filled and show payment section
+function showPaymentDrawer() {
+}
+
+//Add input masks to the checkout page for data validation
+function cardInputMasks() {
+    $("#cardNumber").inputmask("9999 9999 9999 9999",{ "placeholder": "" });
+    $("#cardExpiry").inputmask("99/99",{ "placeholder": "mm/yy" });
+    $("#cardCvv").inputmask("999",{ "placeholder": "" });
+}
+
+function cardPreviewAndFlip() {
+    $("#cardNumber").on("keyup change input paste blur", function () {
+        ccNumber = this.value;
+        $('.cardPreview__number').text(ccNumber);
+    });
+
+    $("#cardName").on("keyup change input paste blur", function () {
+        ccName = this.value;
+        $('.cardPreview__name').text(ccName);
+    });
+
+    $("#cardExpiry").on("keyup change input paste blur", function () {
+        ccExpiry = this.value;
+        $('.cardPreview__date').text(ccExpiry);
+    });
+
+    $("#cardCvv").on("keyup change input paste focus blur", function () {
+        $('.cardPreview__card').addClass('flipped');
+        ccExpiry = this.value;
+        $('.cardPreview__cvv').text(ccExpiry);
+    });
+
+    $("#cardCvv").blur(function(){
+        $('.cardPreview__card').removeClass('flipped');
+    });
+}
