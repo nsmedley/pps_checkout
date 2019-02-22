4e68a51eddc500f3a92e96242a583b1d3dd30395
nsmedley <smed121@gmail.com>
2019-02-22 10:14:37 +0000

    Change

--- js/ppsCheckout.js
+++ js/ppsCheckout.js
@@ -24,7 +24,7 @@ function showPaymentDrawer() {
 //Add input masks to the checkout page for data validation
 function cardInputMasks() {
     $("#cardNumber").inputmask("9999 9999 9999 9999",{ "placeholder": "" });
-    $("#cardExpiry").inputmask("99/99",{ "placeholder": "mm/yy" });
+    $("#cardExpiry").inputmask("99/9999",{ "placeholder": "mm/yyyy" });
     $("#cardCvv").inputmask("999",{ "placeholder": "" });
 }
 
