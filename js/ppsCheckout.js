7efd0957a7dbd6437fc5aa23d574e16ed4874bfd
nsmedley <smed121@gmail.com>
2019-02-26 15:12:59 +0000

    changes

--- js/ppsCheckout.js
+++ js/ppsCheckout.js
@@ -1,56 +1,70 @@
 $(function() {
-    hideInputLabels();
-    cardPreviewAndFlip();
-    checkoutPaymentTabs();
+	hideInputLabels();
+	cardPreviewAndFlip();
+	checkoutPaymentTabs();
+	formValidationClasses();
 });
 
 //If input field has a value then hide the label
 function hideInputLabels() {
-    $('input').focusout(function(){
-        var fieldId = $(this).attr("id");
-        if ($(this).val() == ''){
-            $("label[for='"+fieldId+"']").removeClass('stay');
-         } else {
-            $("label[for='"+fieldId+"']").addClass('stay');
-         }
-    });
+	$("input").focusout(function() {
+		var fieldId = $(this).attr("id");
+		if ($(this).val() == "") {
+			$("label[for='" + fieldId + "']").removeClass("stay");
+		} else {
+			$("label[for='" + fieldId + "']").addClass("stay");
+		}
+	});
 }
 
 function cardPreviewAndFlip() {
-    $("#cardNumber").on("keyup change input paste blur", function () {
-        ccNumber = this.value;
-        $('.cardPreview__number').text(ccNumber);
-    });
+	$("#cardNumber").on("keyup change input paste blur", function() {
+		ccNumber = this.value;
+		$(".cardPreview__number").text(ccNumber);
+	});
 
-    $("#cardName").on("keyup change input paste blur", function () {
-        ccName = this.value;
-        $('.cardPreview__name').text(ccName);
-    });
+	$("#cardName").on("keyup change input paste blur", function() {
+		ccName = this.value;
+		$(".cardPreview__name").text(ccName);
+	});
 
-    $("#cardExpiry").on("keyup change input paste blur", function () {
-        ccExpiry = this.value;
-        $('.cardPreview__date').text(ccExpiry);
-    });
+	$("#cardExpiry").on("keyup change input paste blur", function() {
+		ccExpiry = this.value;
+		$(".cardPreview__date").text(ccExpiry);
+	});
 
-    $("#cardCvv").on("keyup change input paste focus blur", function () {
-        $('.cardPreview__card').addClass('flipped');
-        ccExpiry = this.value;
-        $('.cardPreview__cvv').text(ccExpiry);
-    });
+	$("#cardCvv").on("keyup change input paste focus blur", function() {
+		$(".cardPreview__card").addClass("flipped");
+		ccExpiry = this.value;
+		$(".cardPreview__cvv").text(ccExpiry);
+	});
 
-    $("#cardCvv").blur(function(){
-        $('.cardPreview__card').removeClass('flipped');
-    });
+	$("#cardCvv").blur(function() {
+		$(".cardPreview__card").removeClass("flipped");
+	});
 }
 
 function checkoutPaymentTabs() {
-    $('.paymentTabs .paymentTabs__tab').click(function() {
-        var tab_id = $(this).data("tab");
+	$(".paymentTabs .paymentTabs__tab").click(function() {
+		var tab_id = $(this).data("tab");
 
-		$('.paymentTabs .paymentTabs__tab').removeClass('paymentTabs__tab--active');
-		$('.tabContent').removeClass('openPaymentMethod');
+		$(".paymentTabs .paymentTabs__tab").removeClass("paymentTabs__tab--active");
+		$(".tabContent").removeClass("openPaymentMethod");
 
-		$(this).addClass('paymentTabs__tab--active');
-		$("#"+tab_id).addClass('openPaymentMethod');
-    });
+		$(this).addClass("paymentTabs__tab--active");
+		$("#" + tab_id).addClass("openPaymentMethod");
+	});
+}
+
+function formValidationClasses() {
+	var inputs = document.querySelectorAll("input, select, textarea");
+
+	inputs.forEach(input => {
+		var className = $(this).attr("class");
+		if ($(this).is(":valid")) {
+			$(this).addClass(className + "--invalid");
+		} else {
+			$(this).addClass(className + "--valid");
+		}
+	});
 }
