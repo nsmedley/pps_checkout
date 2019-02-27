0adfb88ab9254175ad249ffd7faa2aa79db0bd0d
nsmedley <smed121@gmail.com>
2019-02-27 14:13:22 +0000

    changes

--- js/ppsCheckout.js
+++ js/ppsCheckout.js
@@ -1,8 +1,8 @@
 $(function() {
 	hideInputLabels();
-	formValidationClasses();
 	cardPreviewAndFlip();
 	checkoutPaymentTabs();
+	formValidation();
 });
 
 //If input field has a value then hide the label
@@ -17,15 +17,28 @@ function hideInputLabels() {
 	});
 }
 
-function formValidationClasses() {
-	document.addEventListener("keyup", function(e) {
-		var input = e.target;
-		if (!$.nodeName(input, "input")) return;
-		input.checkValidity();
-		var element = $(input);
-		if (input.validity.valid) {
-			element.addClass("formFieldValid");
-			element.removeClass("formFieldError");
+function formValidation() {
+	//Email Form validation
+	$("#emailFormContain").validate({
+		rules: {
+			// The key name on the left side is the name attribute
+			// of an input field. Validation rules are defined
+			// on the right side
+			email: "required",
+			email: {
+				required: true,
+				email: true
+			}
+		},
+		// Specify validation error messages
+		messages: {
+			email: "Please enter a valid email address"
+		},
+		errorElement: "span",
+		// Make sure the form is submitted to the destination defined
+		// in the "action" attribute of the form when valid
+		submitHandler: function(form) {
+			form.submit();
 		}
 	});
 }
